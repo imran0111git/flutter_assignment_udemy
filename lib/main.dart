985eeaa524f604bfc58d3899f0834762e4e9bcc5
imran.0111git <imran.md0111@gmail.com>
2020-04-18 19:09:21 +0530

    Added custom icon for app

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Foody',
       theme: ThemeData(
         // This is the theme of your application.
         //
